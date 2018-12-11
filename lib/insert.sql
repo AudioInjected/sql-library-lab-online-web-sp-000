7e59ca12116c71d15d9b5b6e883f606a27ae07cd
Rafael Batista <rafaelb24@yahoo.com>
2018-12-11 16:53:29 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO series (title, author_id, subgenre_id)
+VALUES ("The Witcher", 1, 1);
+
+INSERT INTO series (title, author_id, subgenre_id)
+VALUES ("Lord of the Rings", 2, 1);
+
+INSERT INTO books (title, year, series_id)
+VALUES ("The Last Wish", 2008, 1);
+
+INSERT INTO books (title, year, series_id)
+VALUES ("Sword of Destiny", 2015, 1);
+
+INSERT INTO books (title, year, series_id)
+VALUES ("The Lady of the Lake", 2017, 1);
+
+INSERT INTO books (title, year, series_id)
+VALUES ("The Fellowship of the Ring", 1954, 2);
+
+INSERT INTO books (title, year, series_id)
+VALUES ("The Two Towers", 1954, 2);
+
+INSERT INTO books (title, year, series_id)
+VALUES ("The Return of the King", 1955, 2);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Geralt of Rivia", "Human(Witcher)", "I hate portals", 1, 1);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Yennefer of Vengerberg", "Quadroon", "The easiest way to avoid getting hurt is to stop caring, but not caring is the hardest thing to do", 1, 1);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Cirilla Fiona Elen Riannon", "Human", "You don't know how it is. To see someone you love die. Because of you, for you.", 1, 1);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Vesemir", "Human(Witcher)", "When they're about to hang you, ask for a glass of water. You never know what might happen before they bring it.", 1, 1);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Gandalf", "Human", "Fly", 2, 2);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Gimli", "Dwarf", "Not the beard!", 2, 2);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Gandalf", "Human", "A wizard is never late", 2, 2);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES ("Gandalf", "Human", "A wizard is never late", 2, 2);
